b7c0ec32c3dac6cef18c0c2981be512037563e82
novneetnov <novneet346iith@gmail.com>
2016-11-23 14:07:03 +0530

    Add Alert styling

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -25,5 +25,16 @@ module Spree::BaseHelper
 		end
 	end
 
+	def flash_messages(opts = {})
+		ignore_types = ["order_completed"].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+
+		flash.each do |msg_type, text|
+			unless ignore_types.include?(msg_type)
+				concat(content_tag(:div, text, class: "flash alert alert-#{msg_type}"))
+			end
+		end
+		nil
+	end
+
 end
 
